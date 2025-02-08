638881a780450d852e558b2536289dae8d8858b5
grimoire <libra@project-grimoire.dev>
2025-02-08 19:07:14 +0900

    D1接続テスト

--- src/entry.py
+++ src/entry.py
@@ -1,4 +1,12 @@
 from js import Response
 
+
 async def on_fetch(request, env):
-    return Response.new("Hello World!")
+    # CloudflareのD1 DBに接続
+    db = env.get_db("d1_db")
+    # testテーブルからレコード取得
+    result = await db.query("SELECT * FROM test_tbl")
+    # レスポンスを返す
+    return Response.new(result)
+
+    # return Response.new("Hello World!")
